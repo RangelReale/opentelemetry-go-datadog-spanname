9086b10f973c033f5acd6d3dc44a05bed9f5bb77
Rangel Reale <rangelspam@gmail.com>
2023-04-12 08:34:18 -0300

    rename

--- contrib/net/http/transport.go
+++ contrib/net/http/transport.go
@@ -1,11 +1,14 @@
 package http
 
 import (
+	"net/http"
+
 	"github.com/RangelReale/opentelemetry-go-datadog-spanname/ddtrace"
 	"go.opentelemetry.io/otel/trace"
 )
 
-// NewTransport should be used with otelhttp.NewTransport
+// NewTransport should be used with otelhttp.NewTransport.
+// Also use TransportSpanNameFormatter with otelhttp.WithSpanNameFormatter for a better span name.
 func NewTransport(opts ...Option) trace.TracerProvider {
 	optns := options{
 		operationName: "http.client.request",
@@ -15,4 +18,10 @@ func NewTransport(opts ...Option) trace.TracerProvider {
 	}
 	return ddtrace.NewTracerProvider(optns.operationName,
 		ddtrace.WithTracerProvider(optns.tracerProvider))
+}
+
+// TransportSpanNameFormatter should be used with otelhttp.WithSpanNameFormatter because
+// the default formatter only outputs "HTTP <method>"
+func TransportSpanNameFormatter(_ string, r *http.Request) string {
+	return r.Method + " " + r.URL.Path
 }
